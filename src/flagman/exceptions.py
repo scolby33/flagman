bb2c5fc7f2867846f53e368e878ea87fff843362
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-07-22 23:29:35 +0200

    Update code style in exceptions.py
    If there's a docstring, you don't need a pass

--- src/flagman/exceptions.py
+++ src/flagman/exceptions.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
+
 """Exceptions for flagman."""
 
 
 class ActionClosed(Exception):
     """The Action is closed and no longer will do anything on a call to `run()`."""
-
-    pass
