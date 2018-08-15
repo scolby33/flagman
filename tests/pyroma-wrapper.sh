a93146bd3afb5b092b80b2e1c998677d83a2003c
Scott Colby <scolby33@gmail.com>
2018-08-15 15:27:17 -0700

    No longer need pyroma-wrapper.sh since Pyroma 2.4.
    Thanks, regebro/pyroma#27!

--- tests/pyroma-wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-pyroma_output="$(pyroma "$@")"
-echo "$pyroma_output"
-rating="$(sed -Ene 's/^Final rating: ([[:digit:]]{1,2})\/10/\1/p' << EOF
-"$pyroma_output"
-EOF
-)"
-[ "$rating" -eq 10 ]
